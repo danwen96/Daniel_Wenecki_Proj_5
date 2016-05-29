53c591eab7cf1fc3dc379820bcc986acc3ef7bdb
danwen96 <daniel.wenecki@gmail.com>
2016-05-29 20:59:06 +0000

    Delete zainstalujipcmd.sh

--- zainstalujipcmd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/nathanweeks/ipcmd/archive/master.zip #pobieramy pliki do instalacji i odpakowujemy je
-unzip ./master.zip
-cd ipcmd-master
-make #instalujemy ipcmdwedlug instrukcji z README
-sudo cp ./bin/* /usr/local/sbin/
-sudo cp -r ./man/* /usr/local/man/
